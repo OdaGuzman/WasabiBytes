b4aa4ca2e065e2c0765f57f933b3214c080665c4
OdaGuzman <odaguz.dev@gmail.com>
2023-07-12 17:39:43 -0700

    Susbcribe refactored

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,62 +1,67 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className="bg-white font-jakarta">
       <div className="flex w-full flex-wrap gap-44 overflow-hidden px-10 py-5">
-
         <h3 className="text-secondary-color text-2xl font-600 cursor-pointer">
           <span className="text-primary-color">WASABI    </span> B Y T E S
         </h3>
 
         <ul className="flex gap-24 list-none pt-2">
-          <li className=""><a href="#menu">M E N U</a></li>
+          <li><a href="#menu">M E N U</a></li>
           <li><a href="#about">A B O U T</a></li>
           <li><a href="#login">L O G I N</a></li>
         </ul>
 
         <ul className="footer__social gap-8 flex justify-center items-center">
-          <li>
+          <li className="flex-center">
             <Image
-              src="assets/facebook.svg"
+              src="/assets/facebook.svg"
               alt="facebook"
-              className='object-contain'
+              className="object-contain"
               width={45}
-              height={45} />
+              height={45}
+            />
           </li>
           <li className="flex-center">
-
             <Image
-              src="assets/instagram.svg"
+              src="/assets/instagram.svg"
               alt="instagram"
               className="object-contain"
               width={45}
-              height={45} />
+              height={45}
+            />
           </li>
 
           <li className="flex-center">
             <Image
-              src="assets/twitter.svg" alt="twitter" className="object-contain"
+              src="/assets/twitter.svg"
+              alt="twitter"
+              className="object-contain"
               width={45}
-              height={45} />
+              height={45}
+            />
           </li>
         </ul>
       </div>
-      <h2 className="flex justify-center items-center text-center text-l py-3">Seasoned by <span className="inline-block">
-
-        <a
-          href="https://www.linkedin.com/in/oda-guzman/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-primary-color font-500 ml-1 mr-1"
-        >
-          Oda
-        </a>
-      </span>
-        with ❤️ 👩🏽‍🍳</h2>
-
+      <h2 className="flex justify-center items-center text-center text-l py-3">
+        Seasoned by
+        <span className="inline-block">
+          <a
+            href="https://www.linkedin.com/in/oda-guzman/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary-color font-500 ml-1 mr-1"
+          >
+            Oda
+          </a>
+        </span>
+        with ❤️ 👩🏽‍🍳
+      </h2>
     </div>
-  )
-}
-export default Footer
+  );
+};
+
+export default Footer;
--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,21 +1,53 @@
-import React from 'react'
+'use client'
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-const Subscribe = () => {
+const SubscriptionForm = () => {
+  const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Perform any necessary validation or submission logic
+    setSubmitted(true);
+  };
+
   return (
-    <div className="subscription flex-center" id="services">
-      <h2 data-aos="flip-down">
-        Get offers stright <br />
+    <div className="subscription overflow-hidden p-16 flex-center flex-col bg-[url('../public/assets/subscribe\_bg.png')] bg-primary-color bg-center bg-no-repeat bg-contain" id="services">
+      <h2 className="font-600 text-7xl leading-[123px] text-white text-center font-playfair">
+        Get offers straight <br />
         to your inbox
       </h2>
-      <p data-aos="fade-up">Sign up for the Sushiman newsletter</p>
+      <p className="font-400 text-lg leading-9 text-white font-jakarta">
+        {submitted ? 'Thank you for subscribing!' : 'Sign up for Wasabi Bytes newsletter'}
+      </p>
 
-      <div className="subscription__form" data-aos="fade-up">
-        <input type="text" placeholder="Enter your email address" />
-        <button>Get Started</button>
+      <div className="w-[480px] gap-2.5 border mt-10 pl-4 pr-3.5 py-3.5 font-500 rounded-[46px] border-[rgba(255,255,255,0.5)]">
+        <form className="flex justify-between items-baseline" onSubmit={handleSubmit}>
+          {!submitted && (
+            <>
+              <input
+                type="text"
+                placeholder="Enter your email address"
+                value={email}
+                onChange={handleInputChange}
+                className="placeholder-white placeholder-opacity-50 mb-2 p-2 flex-1 rounded outline-none opacity-50 bg-transparent font-jakarta text-white font-400 text-base"
+              />
+              <button
+                type="submit"
+                className="bg-secondary-color bg-opacity-80 font-jakarta text-white text-center opacity-100 rounded-full p-5 min-w-[180px] text-base outline-none font-600"
+              >
+                Get Started
+              </button>
+            </>
+          )}
+        </form>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Subscribe
+export default SubscriptionForm;
