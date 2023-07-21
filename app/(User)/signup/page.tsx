a613861aa277b55fd7f7247e4efdf5826687934e
OdaGuzman <odaguz.dev@gmail.com>
2023-07-21 01:10:22 -0700

    sign up page - 60%

--- app/(User)/signup/page.tsx
+++ app/(User)/signup/page.tsx
@@ -1,43 +1,103 @@
-import Header from './Header'
-import Footer from '../../../components/Footer'
-import React from 'react'
-import Image from 'next/image'
+'use client'
+import React, { useState } from 'react';
+import Header from '../signup/Header';
+import Image from 'next/image';
 
+const SignUpForm: React.FC = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+  });
 
-const Menu = () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Add your sign-up form submission logic here
+    console.log(formData);
+    // You can use the formData object to send data to your backend or perform any other actions.
+  };
+
   return (
-    <div>
+    <>
       <Header />
-
-      <div className="relative flex h-screen">
-        {/* Background */}
-        <div className="flex absolute inset-0 bg-primary-color">
-
-          {/* Image */}
-          <div className="flex items-center justify-center w-1/2">
-            <Image
-              src="/assets/signup1.png"
-              alt="Image"
-              className=""
-              width={550}
-              height={250} />
-          </div>
-
-          {/* Form */}
-          <div className="flex items-center justify-center w-1/2 bg-opacity-50" >
-            <div className="bg-white p-8 rounded-lg">
-              <h2 className="text-4xl text-center font-playfair">Sign Up / Login</h2>
-              <form>
-                {/* Add your form fields here */}
-              </form>
-            </div>
-          </div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        {/* Hero Banner Image */}
+        <div className="w-full h-screen mb-6 md:w-1/2 md:mx-auto">
+          <Image
+            src="/assets/signup2.png"
+            className="object-cover w-full h-full"
+            alt="Image"
+            layout="fill"
+          />
         </div>
       </div>
 
-      <Footer />
-    </div>
-  )
-}
+      {/* Form */}
+      <h2 className="flex items-center text-center lg:text-4xl font-bold text-primary-color font-jakarta pb-10">Create your account</h2>
+      <form onSubmit={handleSubmit} className="w-1/2">
+        <div className="mb-4">
+          <label htmlFor="lastName" className="block mb-2 font-semibold">
+            First Name
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            placeholder='Enter your first name'
+            className="w-full px-4 py-2 border rounded-full focus:outline-none focus:border-primary-color"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="lastName" className="block mb-2 font-semibold">
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            placeholder='Enter your last name'
+            className="w-full px-4 py-2 border rounded-full focus:outline-none focus:border-primary-color"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block mb-2 font-semibold">
+            E-mail
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder='Enter your email address'
+            name="email"
+            className="w-full px-4 py-2 border rounded-full focus:outline-none focus:border-primary-color"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-export default Menu
+        {/* Add the rest of the form fields similarly */}
+        <button
+          type="submit"
+          className="flex w-full px-4 py-2 text-white bg-primary-color rounded-full hover:bg-opacity-80 focus:outline-none"
+        >
+          Sign Up
+        </button>
+      </form>
+
+    </>
+  );
+};
+
+export default SignUpForm;
