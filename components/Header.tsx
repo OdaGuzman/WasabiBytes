093f13e3767b26ae470ced5437fe6710aadbd917
OdaGuzman <odaguz.dev@gmail.com>
2023-08-01 17:17:33 -0700

    Header g - modified

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
 import Image from 'next/image'
 import Link from 'next/link'
+
 const Header = () => {
+
+  const { user } = useUser()
   return (
+
 
     <div className="flex overflow-hidden font-jakarta-600" >
       <div className="flex-1 relative p-5 bg-primary-color">
@@ -13,13 +18,14 @@ const Header = () => {
         <div className="absolute w-full z-[-1] inset-0;"></div>
       </div>
 
+
       <ul className="flex-[1.236] flex items-center justify-end gap-8 p-5 list-none font-medium text-secondary-color text-base uppercase leading-5">
         <li>
           <Link href="/menu">Menu</Link>
         </li>
         {/* <li>
-          <a href="../app/about/page.tsx">About</a>
-        </li> */}
+         <a href="../app/about/page.tsx">About</a>
+       </li> */}
         <li className="cursor-pointer">
           <a href="/cart">
             <Image
@@ -31,23 +37,30 @@ const Header = () => {
             />
           </a>
         </li>
-
-        <li className="cursor-pointer">
-          <a href="/signup">
-            <Image
-              src="/assets/userhat.png"
-              alt="User Log In"
-              title="User Log In"
-              width={40}
-              height={40}
-            />
-          </a>
-        </li>
+        {user ?
+          <li>
+            <Link href="/api/auth/logout" >
+              <p>Logout</p>
+            </Link>
+          </li>
+          :
+          <li>
+            <Link href="/api/auth/login">
+              <Image
+                src="/assets/userhat.png"
+                alt="User Log In"
+                title="User Log In"
+                width={40}
+                height={40}
+              />
+            </Link>
+          </li>
+        }
       </ul>
     </div >
 
-
   )
 }
+
 
 export default Header
