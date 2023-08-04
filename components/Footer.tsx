56cc63a059f1040269880cfbba5bda92879ce94a
OdaGuzman <odaguz.dev@gmail.com>
2023-08-04 01:28:19 -0700

    patch 2.1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,19 +3,28 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div className="bg-white font-jakarta">
+    <div className="bg-primary-color font-jakarta">
       <div className="flex flex-col lg:flex-row w-full flex-wrap justify-between overflow-hidden px-10 py-5">
         <h3 className="text-secondary-color text-md lg:text-2xl font-600">
           <a href="/">
-            <span className="text-primary-color">WASABI    </span> B Y T E S
+            <span className="text-white">WASABI    </span> B Y T E S
           </a>
         </h3>
 
-        <ul className="flex gap-12 lg:gap-24 list-none pt-2 text-sm lg:text-xl w-full lg:w-auto justify-between">
-          {/* <li><a href="../app/menu/page.tsx">M E N U</a></li>
-          <li><a href="../app/(User)/signup">L O G I N</a></li> */}
-        </ul>
 
+        <h2 className="flex justify-center text-white items-center text-center text-l py-3">
+          Seasoned by
+          <span className="inline-block">
+            <a
+              href="https://www.linkedin.com/in/oda-guzman/"
+              target="_blank"
+              className=" text-blue-800 font-500 ml-1 mr-1"
+            >
+              Oda
+            </a>
+          </span>
+          <span className="text-white">with ❤️ 👩🏽‍🍳 </span>
+        </h2>
         <ul className="lg:gap-8 flex lg:justify-center items-center pt-4 lg:pt-0 w-full lg:w-auto justify-between ml-1 lg:ml-0 pr-4 lg:pr-0">
           <li className="flex-center bg-black rounded-full h-8 w-8">
             <Image
@@ -45,21 +54,11 @@ const Footer = () => {
               height={45}
             />
           </li>
+
         </ul>
+
       </div>
-      <h2 className="flex justify-center items-center text-center text-l py-3">
-        Seasoned by
-        <span className="inline-block">
-          <a
-            href="https://www.linkedin.com/in/oda-guzman/"
-            target="_blank"
-            className="text-blue-600 font-500 ml-1 mr-1"
-          >
-            Oda
-          </a>
-        </span>
-        with ❤️ 👩🏽‍🍳
-      </h2>
+
     </div >
   );
 };
